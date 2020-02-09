65754bfc3eb2301fabe691900897454d9ec4a26e
Gypsy <danarellano@protonmail.com>
2020-02-09 10:10:22 -0500

    HW3 working, 3/3 Completed, Clean up pending

--- HW3_DanielArellano.py
+++ HW3_DanielArellano.py
@@ -82,35 +82,66 @@ print('Done.')
 print('\nQuestion 2:')
 shelly.clear()
 
+print('Drawing 1st circle...')
+# Create spacing (gap) between circles
 shelly.forward(50)
 shelly.pendown()
+# Draw 1st circle (radius = 50)
 shelly.left(90)
 shelly.circle(50)
 shelly.right(90)
+print('Done.')
 
+print('Drawing 2nd circle...')
+# Create spacing (gap) between circles
 shelly.penup()
 shelly.forward(50)
 shelly.pendown()
+# Draw 2nd circle (radius = 100)
 shelly.left(90)
 shelly.circle(100)
 shelly.right(90)
+print('Done.')
 
+print('Drawing 3rd circle...')
+# Create spacing (gap) between circles
 shelly.penup()
 shelly.forward(50)
 shelly.pendown()
+# Draw 3rd circle (radius = 150)
 shelly.left(90)
 shelly.circle(150)
 shelly.right(90)
+print('Done.')
 
+print('Drawing 4th circle...')
+# Create spacing (gap) between circles
 shelly.penup()
 shelly.forward(50)
 shelly.pendown()
+# Draw 4th circle (radius = 200)
 shelly.left(90)
 shelly.circle(200)
 shelly.right(90)
+print('Done.')
 
+print('Reseting...')
+# Shelly, go back home. (reset)
 shelly.penup()
 shelly.setposition(0,0)
+print('Done.')
+
+
+### QUESTION 3 ###
+print('\nQuestion 3:')
+
+a = math.factorial(100)
+print('  a)  100! = ' + str(a))
+b = math.log2(1000000)
+print('  b)  The log(base 2) of 1,000,000 is ' + str(b))
+c = math.gcd(69,9)
+print('  c)  The greatest common divisor of 63 and 49 is ' + str(c))
+
 
 
 input('\n\nPress enter to exit...')
