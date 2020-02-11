70a26aa5ec853186b23df98d943289e61ed8a922
Gypsy <danarellano@protonmail.com>
2020-02-11 15:45:35 -0500

    Hw4 2/3 completed

--- HW4_DanielArellano.py
+++ HW4_DanielArellano.py
@@ -5,6 +5,70 @@ HW 04, February 11, 2020
 '''
 
 ### QUESTION 1 ###
-print('Question 1:')
+print('Question 1:\n')
+
+a = 3
+b = 4
+c = 5
+
+print('a = ' + str(a))
+print('b = ' + str(b))
+print('c = ' + str(c))
+
+print('\nIs a less than b?')
+if a < b:
+    print('  OK')
+
+print('\nIs c less than b?')
+if c < b:
+    print('  OK')
+
+print('\nIs the sum of a and b equal to c?')
+if a + b == c:
+    print('  OK')
+
+print('\nIs the sum of a^2 and b^2 equal to c^2?')
+if a**2 + b**2 == c**2:
+    print('  OK')
 
 
+### QUESTION 2 ###
+print('\n\nQuestion 2:\n')
+
+a = 3
+b = 4
+c = 5
+
+print('a = ' + str(a))
+print('b = ' + str(b))
+print('c = ' + str(c))
+
+print('\nIs a less than b?')
+if a < b:
+    print('  OK')
+else:
+    print('  NOT OK')
+
+print('\nIs c less than b?')
+if c < b:
+    print('  OK')
+else:
+    print('  NOT OK')
+
+print('\nIs the sum of a and b equal to c?')
+if a + b == c:
+    print('  OK')
+else:
+    print('  NOT OK')
+
+print('\nIs the sum of a^2 and b^2 equal to c^2?')
+if a**2 + b**2 == c**2:
+    print('  OK')
+else:
+    print('  NOT OK')
+
+
+### Question 3 ###
+
+
+input('\n\nPress enter to exit...')
